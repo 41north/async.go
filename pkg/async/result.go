65b54ea5d371a20918ee251c704a803d431d6a81
Brian McGee <brian@41north.dev>
2022-09-06 13:15:01 +0200

    doc: add godoc and examples for Future and Result

--- pkg/async/result.go
+++ pkg/async/result.go
@@ -9,10 +9,12 @@ func (r result[T]) Unwrap() (T, error) {
 	return r.value, r.err
 }
 
+// NewResult creates a successful result.
 func NewResult[T any](value T) Result[T] {
 	return result[T]{value: value}
 }
 
+// NewResultErr creates a failed result.
 func NewResultErr[T any](err error) Result[T] {
 	return result[T]{err: err}
 }
